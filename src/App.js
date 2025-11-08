62f0d379f43796a0947bb15b6d1397652cc31f32
KiiwiS <KiiwiStrawberry@gmail.com>
2025-11-07 22:18:00 -0500

    feat: Initial React project setup with base configuration

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GameTracker</h1>
+      </header>
+      <main>
+        <section className="games-grid">
+          {/* Aquí irán los juegos */}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
